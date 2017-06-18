59470d4aba7ab6fc2546f3e49547ee35fcd46f83
Nutthaphon Suwanwong <nutthaphon@gmail.com>
2017-06-18 22:12:13 +0700

    ok

--- js/netpie.global.js
+++ js/netpie.global.js
@@ -44,6 +44,35 @@ function saveTheme(){
             datajson.theme = np_theme;
             window.localStorage.setItem("netpie.freeboard.dashboard", JSON.stringify(datajson));
 
+            freeboard.on('load_theme',function() {
+                var stylesheet = document.getElementById('netpie-theme-css');
+                var data = window.localStorage.getItem("netpie.freeboard.dashboard");
+                var datajson = JSON.parse(data);
+                if(datajson!==null){
+                    if(datajson.theme===null || datajson.theme=="default"){
+                        if(stylesheet!=null){
+                            stylesheet.parentNode.removeChild(stylesheet);
+                        }
+                        np_theme = "default";
+                        document.getElementById('theme-toggle').checked = false;
+                    }
+                    if(datajson.theme=="netpie"){
+                        var  theme = document.createElement('link');
+                        theme.id = 'netpie-theme-css';
+                        theme.href = 'css/netpie.theme.css';
+                        theme.rel = 'stylesheet';
+                        document.head.appendChild(theme);
+                        np_theme = "netpie";
+                        document.getElementById('theme-toggle').checked = true;
+                    }
+                }
+                else{
+                    np_theme = "default";
+                    document.getElementById('theme-toggle').checked = false;
+                }
+                saveTheme();
+            });
+
             freeboard.emit('load_theme');
         }
     };
@@ -53,35 +82,6 @@ function saveTheme(){
 
 
 }
-
-freeboard.on('load_theme',function() {
-    var stylesheet = document.getElementById('netpie-theme-css');
-    var data = window.localStorage.getItem("netpie.freeboard.dashboard");
-    var datajson = JSON.parse(data);
-    if(datajson!==null){
-        if(datajson.theme===null || datajson.theme=="default"){
-            if(stylesheet!=null){
-                stylesheet.parentNode.removeChild(stylesheet);
-            }
-            np_theme = "default";
-            document.getElementById('theme-toggle').checked = false;
-        }
-        if(datajson.theme=="netpie"){
-            var  theme = document.createElement('link');
-            theme.id = 'netpie-theme-css';
-            theme.href = 'css/netpie.theme.css';
-            theme.rel = 'stylesheet';
-            document.head.appendChild(theme);
-            np_theme = "netpie";
-            document.getElementById('theme-toggle').checked = true;
-        }
-    }
-    else{
-        np_theme = "default";
-        document.getElementById('theme-toggle').checked = false;
-    }
-    saveTheme();
-});
 
 
 
